4cc945d8f7bb85443e24a188f02081e0e01b31e6
Ilkin <105630796+ilkin0316@users.noreply.github.com>
2023-12-02 09:27:11 +0400

    Player Animation V1
    Player Animation V1

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z -11.19f);
+    }
+}
